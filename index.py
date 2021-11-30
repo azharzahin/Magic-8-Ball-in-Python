bf07e3f81a1e4063e42e2d848485444d81c33426
azharzahin <18713606+azharzahin@users.noreply.github.com>
2021-11-30 21:15:10 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,42 @@
+import random
+name = "aynur"
+question = "will i get a developer job after python programming?"
+answer = ""
+
+random_number = random.randint(1,9)
+
+print(random_number)
+#we used here a predefined method .randint
+# now we are gonna to use control statement
+if random_number == 1:
+ answer = "Yes = definitely."
+
+elif random_number == 1:
+ answer = "It is decidedly so."
+elif random_number == 1:
+ answer = "Without a doubt."
+elif random_number == 1:
+ answer = "Reply hazy, try again."
+elif random_number == 1:
+ answer = "Ask again later."
+elif random_number == 1:
+ answer = "Better not tell you now."
+elif random_number == 1:
+ answer = "My sources say no."
+elif random_number == 1:
+ answer = "Outlook not so good."
+elif random_number == 1:
+ answer = "Very doubtful."
+
+else:
+ answer = "Error"
+
+print(name + "asks:" + question)
+
+print("Magic 8-Ball's answer:" + (answer))
+
+#task done understand this project properly and rest task was recommending me for practices and so if u will learn python u will undertand it better for more practice u can do a lot of things as well time is 8:30pm right now date is 27nove 2021 its day 67as well babye
+
+
+
+
